87c0f15682e6b00105e988d81a80405100e07be9
Ifrat Binte <ifratbinte@gmail.com>
2023-03-28 16:12:40 +0600

    feat (*) : add service with dynamic data

---
+++ src/components/Service/index.tsx
@@ -0,0 +1,22 @@
+import serviceData from "#mocks/service.json";
+import TextPlate from "../common/TextPlate";
+
+const Service = () => {
+  const service = serviceData.service;
+  return (
+    <div className="section-gap relative bg-grayPrimary">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-5">
+          <div className="lg:w-5/12">
+            <h3 className="text-3xl font-integralCF font-bold lg:w-3/4">{service.title}</h3>
+          </div>
+          {service.item.map((service: any, i: number) => {
+            return <TextPlate key={i} icon={service.icon}  title={service.title} content={service.content}/>
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,12 @@ import styles from '#/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 import Hero from "#components/Hero"
+import Service from '#/components/Service'
 export default function Home() {
   return (
     <>
       <Hero/>
+      <Service/>
     </>
   )
 }
