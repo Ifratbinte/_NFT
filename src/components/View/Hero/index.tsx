fad0cf49907427cd1beecc5baa94405734b3de47
Ifrat Binte <ifratbinte@gmail.com>
2023-03-31 15:42:19 +0600

    feat()* : add template literal for banner

--- src/components/View/Hero/index.tsx
+++ src/components/View/Hero/index.tsx
@@ -2,47 +2,38 @@ import Button from "#components/Button/btnPrimary";
 import heroData from "#mocks/hero.json";
 import { FaEthereum } from "react-icons/fa";
 import styles from "./hero.module.css";
+import Banner from "#/components/common/Banner";
 
 const Hero = () => {
   const hero = heroData.hero;
   return (
-    <div className={`${styles.hero} container mx-auto md:h-70v relative section-b-gap-s mt-10 lg:mt-44`}>
-      <div className="flex items-center flex-col lg:flex-row-reverse gap-10">
+    <div className={`${styles.hero} container mx-auto md:h-70v relative section-b-gap-s lg:mt-36 mt-10`}>
+      <div className="flex items-center justify-between flex-col lg:flex-row-reverse gap-10 h-4/5">
+        
         <div className="relative flex w-full">
-          <div className="absolute left-64 z-50 top-28">
+          <div className="absolute left-20 z-50 top-36">
             <img src={hero.badge} alt="" />
           </div>
+          <div className="flex justify-center items-center">
           {hero.banner.map((banner: any, i: number) => {
             return (
-              <div className="flex justify-center items-center" key={i}>
-                <div className={`${i === 0 && "z-30 left-80" } ${i === 1 && "z-20 left-20" } ${i === 2 && "z-10 right-32" } relative`}>
-                  <img src={banner.image} alt="" />
-                  <div className="absolute top-0 w-full h-full flex flex-col justify-between py-6 lg:px-10 px-5 ">
-                    <div>
-                      <h4 className="text-2xl text-white font-semibold font-DmSans">{banner.title}</h4>
-                      <div className="flex items-center py-3">
-                        <img src={banner.author} alt={banner.author_name} />
-                        <span className="text-lg text-white pl-3 font-DmSans">{banner.author_name}</span>
-                      </div>
-                    </div>
-                    <ul className="flex justify-between flex-wrap py-3 px-4 rounded-xl backdrop-blur-md bg-white/30">
-                      <li className="w-1/2 text-sm text-slate-50 font-DmSan pb-2">{banner?.badge?.title}</li>
-                      <li className="w-1/2 text-sm text-slate-50 font-DmSans pb-2">{banner?.badge?.ends_time}</li>
-                      <li className="w-1/2 text-slate-50 flex items-center font-DmSans">
-                        <FaEthereum className="mr-2" />
-                        {banner?.badge?.bid_price}
-                      </li>
-                      <li className="w-1/2 text-slate-50 font-DmSans">{banner?.badge?.bid_hour}</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+              <Banner 
+                id={i}
+                image={banner.image} 
+                author={banner.author} 
+                author_name={banner.author_name} 
+                title={banner.title} 
+                ends_time={banner?.badge?.ends_time} 
+                badge_title={banner?.badge?.title} 
+                bid_price={banner?.badge?.bid_price} 
+                bid_hour={banner?.badge?.bid_hour}
+              />
             );
           })}
+          </div>
         </div>
-        
-        <div className="w-full lg:w-1/2 text-center lg:text-left">
-          <h2 className="text-2xl md:text-3xl lg:text-4xl font-integralCF mb-5">{hero.title}</h2>
+        <div className="w-full lg:w-3/4 text-center lg:text-left">
+          <h2 className="text-2xl md:text-3xl lg:text-5xl font-integralCF mb-5">{hero.title}</h2>
           <p className="text-lg font-DmSans text-paragraph mb-9">{hero.content}</p>
           <Button btn_name={hero.btn_name} />
           <ul className="flex items-center lg:justify-between justify-center lg:w-1/2 mt-6">
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       colors: {
         primary: "#3D00B7",
         grayPrimary: "#F7F9FB",
+        grayPrimaryLight: "#F2F3F5",
         paragraph: "#696969",
         danger: "#FF002E",
         blue: "#2639ED"
@@ -26,7 +27,18 @@ module.exports = {
       },
       height: {
         "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "1000v": "100vh",
+        "500" : "500px",
+        "440" : "440px",
+        "380" : "380px",
       },
+      width: {
+        "800" : "800px",
+        "260" : "260px",
+        "370" : "370px"
+      }
     },
   },
   plugins: [
