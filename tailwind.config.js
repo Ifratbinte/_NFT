a5adcc3b333181345b1d1413b208cb06e67ea447
Ifrat Binte <ifratbinte@gmail.com>
2023-04-13 16:25:22 +0600

    refactor image

--- tailwind.config.js
+++ tailwind.config.js
@@ -52,6 +52,16 @@ module.exports = {
         "120px": "120px",
         "75px": "75px",
       },
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [
